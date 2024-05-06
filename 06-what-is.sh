5618165343d3ef85c132c0a7878bde5aefaa7aea
@k33g <ph.charriere@gmail.com>
2024-05-06 20:31:00 +0200

    💾 Saved.

---
+++ 06-what-is.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+OLLAMA_URL="http://localhost:11434"
+MODEL="k33gllama"
+
+read -r -d '' USER_CONTENT <<- EOM
+What is the position of Philippe Charrière in the USS Enterprise?
+EOM
+
+USER_CONTENT=$(echo ${USER_CONTENT} | tr -d '\n')
+
+read -r -d '' DATA <<- EOM
+{
+  "model":"${MODEL}",
+  "options": {
+    "temperature": 0.5,
+    "repeat_last_n": 2
+  },
+  "prompt": "${USER_CONTENT}",
+  "stream": false
+}
+EOM
+
+curl ${OLLAMA_URL}/api/generate \
+   -H "Content-Type: application/json" \
+   -d "${DATA}" | jq -c '{ response }'
+
+
+
---
+++ 07-pizza.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+OLLAMA_URL="http://localhost:11434"
+MODEL="k33gllama"
+
+read -r -d '' USER_CONTENT <<- EOM
+according the context, what is the best pizza of the world?
+EOM
+
+USER_CONTENT=$(echo ${USER_CONTENT} | tr -d '\n')
+
+read -r -d '' DATA <<- EOM
+{
+  "model":"${MODEL}",
+  "options": {
+    "temperature": 0.1,
+    "repeat_last_n": 2
+  },
+  "prompt": "${USER_CONTENT}",
+  "stream": false
+}
+EOM
+
+curl ${OLLAMA_URL}/api/generate \
+   -H "Content-Type: application/json" \
+   -d "${DATA}" | jq -c '{ response }'
+
+
+
